ddb9644695774a793e86179fa29eb2fb891c9fe1
Mark <mark.eibes@gmail.com>
2026-05-25 00:37:24 +0200

    markgraf-www: dark-theme the markgraf renderer with a CSS invert+hue-rotate filter

--- src/Page/HeroPreview.purs
+++ src/Page/HeroPreview.purs
@@ -202,7 +202,6 @@ previewPane src size =
                 , minHeight: "0"
                 , position: "relative"
                 , overflow: "hidden"
-                , filter: "invert(0.92) hue-rotate(180deg)"
                 }
             , children:
                 if size.w <= 0.0 || size.h <= 0.0
