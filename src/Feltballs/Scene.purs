1cb21bfda2f1c032132b876cb37d6be08f781b18
Mark <mark.eibes@gmail.com>
2026-05-28 12:35:30 +0200

    ai/tornado: bell-shaped walls (sqrt curve) instead of inward V

--- src/Feltballs/Scene.purs
+++ src/Feltballs/Scene.purs
@@ -520,7 +520,7 @@ tornadoPos t f i = { x: bx + jx, y: by + jy, z: bz + jz }
   floorEnd = 0.25
   suckEnd = 0.4
   helixU = clamp01 ((u - suckEnd) / (1.0 - suckEnd))
-  turns = 2.5
+  turns = 5.0
   -- Bell-shaped funnel: walls bow outward like )(. sqrt gives a pointy tip
   -- with fast initial flare that eases off toward the crown.
   funnel = 0.05 + sqrt helixU * 1.2
