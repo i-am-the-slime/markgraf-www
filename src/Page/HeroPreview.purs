036df3aef9ee4c739ff87918361fd145c2650d93
Mark <mark.eibes@gmail.com>
2026-05-29 15:22:01 +0200

    hero: keep wordmark lowercase

--- src/Page/HeroPreview.purs
+++ src/Page/HeroPreview.purs
@@ -266,9 +266,9 @@ heroLockup =
             }
             [ span
                 { className: "vhs-text"
-                , style: css { "--vhs-text": "\"Markgraf\"" }
+                , style: css { "--vhs-text": "\"markgraf\"" }
                 }
-                [ text "Markgraf" ]
+                [ text "markgraf" ]
             ]
         , p
             { className: "max-w-[34ch] text-[clamp(1.125rem,2.2vw,2rem)] leading-snug text-[#f5f1e8]"
