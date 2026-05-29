a498301f075607267b105f5e3b154e45f7c08ec0
Mark <mark.eibes@gmail.com>
2026-05-29 09:57:56 +0200

    scanlines: move from text overlay to page background

--- src/Feltballs/Bindings.purs
+++ src/Feltballs/Bindings.purs
@@ -1,21 +1,24 @@
-module Feltballs.Bindings where
+module Feltballs.Bindings
+  ( module Yoga.React.R3F.Events
+  , module Drei
+  , module DreiStaging
+  , boxGeometry
+  , planeGeometry
+  , sphereGeometry
+  , icosahedronGeometry
+  , cylinderGeometry
+  , meshBasicMaterial
+  , meshLambertMaterial
+  , meshPhongMaterial
+  , meshStandardMaterial
+  ) where
 
-import Prelude
-
-import Data.Function.Uncurried (Fn2, runFn2)
-import Effect (Effect)
-import React.Basic (JSX, ReactComponent, element)
+import React.Basic (JSX, element)
 import React.R3F.Three.Internal (threejs)
+import Yoga.React.R3F.Drei.Misc (edges, outlines, instances, instance_, roundedBoxGeometry, text, html) as Drei
+import Yoga.React.R3F.Drei.Staging (environment) as DreiStaging
+import Yoga.React.R3F.Events (ThreeEvent, eventPointX, eventPointY, eventPointZ, stopPropagation, withChildren)
 
--- ThreeEvent FFI (R3F pointer events)
-foreign import data ThreeEvent :: Type
-
-foreign import eventPointX :: ThreeEvent -> Number
-foreign import eventPointY :: ThreeEvent -> Number
-foreign import eventPointZ :: ThreeEvent -> Number
-foreign import stopPropagation :: ThreeEvent -> Effect Unit
-
--- Built-in R3F intrinsics via the `threejs` helper (string → component name)
 boxGeometry :: forall p. { | p } -> JSX
 boxGeometry = element (threejs "BoxGeometry")
 
@@ -42,50 +45,3 @@ meshPhongMaterial = element (threejs "MeshPhongMaterial")
 
 meshStandardMaterial :: forall p. { | p } -> JSX
 meshStandardMaterial = element (threejs "MeshStandardMaterial")
-
--- Drei components (FFI to native React components)
-foreign import edgesImpl :: forall a. ReactComponent { | a }
-
-edges :: forall p. { | p } -> JSX
-edges = element edgesImpl
-
-foreign import outlinesImpl :: forall a. ReactComponent { | a }
-
-outlines :: forall p. { | p } -> JSX
-outlines = element outlinesImpl
-
-foreign import instancesImpl :: forall a. ReactComponent { | a }
-
-instances :: forall p. { | p } -> Array JSX -> JSX
-instances p kids = element instancesImpl (withChildren kids p)
-
-foreign import instanceImpl :: forall a. ReactComponent { | a }
-
-instance_ :: forall p. { | p } -> JSX
-instance_ = element instanceImpl
-
-foreign import roundedBoxGeometryImpl :: forall a. ReactComponent { | a }
-
-roundedBoxGeometry :: forall p. { | p } -> JSX
-roundedBoxGeometry = element roundedBoxGeometryImpl
-
-foreign import environmentImpl :: forall a. ReactComponent { | a }
-
-environment :: forall p. { | p } -> JSX
-environment = element environmentImpl
-
-foreign import textImpl :: forall a. ReactComponent { | a }
-
-text :: forall p. { | p } -> JSX
-text = element textImpl
-
-foreign import htmlImpl :: forall a. ReactComponent { | a }
-
-html :: forall p. { | p } -> JSX
-html = element htmlImpl
-
--- Helpers
-foreign import withChildrenImpl :: forall c p. Fn2 c p p
-
-withChildren :: forall c p. c -> p -> p
-withChildren = runFn2 withChildrenImpl
