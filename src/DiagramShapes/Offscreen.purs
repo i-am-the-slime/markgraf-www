67bbbef004b13364b43afd49fd2abc9cb9c7be59
Mark <mark.eibes@gmail.com>
2026-05-29 14:57:26 +0200

    rebuild artifacts after dropping rotated cursor

--- src/DiagramShapes/Offscreen.purs
+++ src/DiagramShapes/Offscreen.purs
@@ -2,29 +2,41 @@ module DiagramShapes.Offscreen (diagramShapesOffscreen) where
 
 import Prelude
 
+import Data.Int (toNumber)
 import Data.Maybe (Maybe(..))
 import Data.Nullable (toNullable)
+import Data.Number (sqrt)
 import Effect (Effect)
 import Effect.Unsafe (unsafePerformEffect)
 import React.Basic (ReactComponent)
 import React.Basic.Hooks (Component, component, readRefMaybe, useEffectOnce, useRef)
 import React.Basic.Hooks as Hooks
 import Unsafe.Coerce (unsafeCoerce)
+import Web.DOM.Element (clientHeight, clientWidth)
+import Web.HTML.HTMLCanvasElement (HTMLCanvasElement, toElement)
 import Yoga.React.DOM.HTML.Canvas (canvas)
 import Yoga.React.DOM.Internal (css, noJSX)
 
-foreign import data CanvasEl :: Type
-foreign import setupDiagramShapesImpl :: Int -> CanvasEl -> Effect (Effect Unit)
+foreign import setupDiagramShapesImpl :: Int -> HTMLCanvasElement -> Effect (Effect Unit)
 
 pixelBudget :: Int
 pixelBudget = 640 * 480
+
+targetDpr :: Int -> HTMLCanvasElement -> Effect Number
+targetDpr budget canvasEl = do
+  w <- clientWidth el
+  h <- clientHeight el
+  pure (min 1.0 (sqrt (toNumber budget / area w h)))
+  where
+  el = toElement canvasEl
+  area w h = max 1.0 w * max 1.0 h
 
 diagramShapesOffscreen :: ReactComponent {}
 diagramShapesOffscreen = unsafeCoerce (unsafePerformEffect diagramShapesOffscreenComponent)
 
 diagramShapesOffscreenComponent :: Component {}
 diagramShapesOffscreenComponent = component "DiagramShapesOffscreen" \_ -> Hooks.do
-  canvasRef <- useRef (toNullable (Nothing :: Maybe CanvasEl))
+  canvasRef <- useRef (toNullable (Nothing :: Maybe HTMLCanvasElement))
 
   useEffectOnce do
     readRefMaybe canvasRef >>= case _ of
